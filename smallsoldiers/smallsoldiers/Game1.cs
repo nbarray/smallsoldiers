87adbeb51846c750c029c7fad1d18bb4fade22dd
nbarray <nbarray@40a03364-7479-0020-56b9-5d5206832d97>
2013-03-22 18:26:00 +0000

    ajout de pixel.png, et des classe Homeland.cs, Slot.cs

--- smallsoldiers/smallsoldiers/Game1.cs
+++ smallsoldiers/smallsoldiers/Game1.cs
@@ -8,16 +8,16 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using smallsoldiers.land;
 
 namespace smallsoldiers
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        Homeland zone_joueur_1, zone_joueur_2;
 
         public Game1()
         {
@@ -25,70 +25,45 @@ namespace smallsoldiers
             Content.RootDirectory = "Content";
             graphics.PreferredBackBufferWidth = 1366;
             graphics.PreferredBackBufferHeight = 768;
+            IsMouseVisible = true;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             Ressource.Initialize(GraphicsDevice, Content);
+            zone_joueur_1 = new Homeland(true);
+            zone_joueur_2 = new Homeland(false);
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
             Ressource.LoadContent();
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
-            // TODO: Add your update logic here
+            zone_joueur_1.Update();
+            zone_joueur_2.Update();
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
 
             Ressource.sb.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
-            Ressource.sb.Draw(Ressource.Get("bg01"), new Rectangle(0, 0, 1366, 768), Color.White);
+            zone_joueur_1.Draw();
+            zone_joueur_2.Draw();
             Ressource.sb.End();
 
             base.Draw(gameTime);
