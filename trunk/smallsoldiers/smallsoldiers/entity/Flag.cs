bc3e1fe8c5f1be76e7545c5a4b2c49955fbf6250
darksteelpenguin@gmail.com <darksteelpenguin@gmail.com@40a03364-7479-0020-56b9-5d5206832d97>
2013-03-22 21:18:18 +0000

    Edit flags

--- trunk/smallsoldiers/smallsoldiers/entity/Flag.cs
+++ trunk/smallsoldiers/smallsoldiers/entity/Flag.cs
@@ -8,12 +8,29 @@ namespace smallsoldiers.entity
 {
     class Flag : Entity
     {
+        private List<Soldier> soldiers_linked;
+
         public Flag(string _asset)
             : base(_asset,
                    new Rectangle(0, 0, Cons.BUILDING_SIZE, Cons.BUILDING_SIZE),
                    new Rectangle(0, 0, Cons.BUILDING_SIZE, Cons.BUILDING_SIZE),
                    Color.White)
         {
+        }
+
+        public void add_new_soldier(Soldier _s)
+        {
+            soldiers_linked.Add(_s);
+        }
+
+        public void set_new_pos(int _x, int _y)
+        {
+            rect.X = _x;
+            rect.Y = _y;
+            foreach (Soldier item in soldiers_linked)
+            {
+                item.move_to(_x, _y);
+            }
         }
     }
 }
